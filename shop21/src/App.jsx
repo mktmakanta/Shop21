96e3a27cbfe45db1254352bda593e7f2df733b0f
mktmakanta <mktmakantamkt@gmail.com>
2024-08-29 16:48:43 +0100

    first commit

---
+++ shop21/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ex.
+      </h1>
+    </>
+  );
+}
+
+export default App;
